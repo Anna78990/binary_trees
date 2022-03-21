ed7db390e0c14086a56db82bae4cdb9332327220
Anna78990 <takasuanna1990@gmail.com>
2022-03-21 13:57:06 +0000

    1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,37 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+	binary_tree_t *left;
+	int left_number;
+
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = value;
+	new->right = NULL;
+	new->left = NULL;
+	if (parent)
+	{
+		if (parent->left)
+		{
+			left = parent->left;
+			left_number = left->n;
+			left->n = value;
+			new->n = left_number;
+			left->left = new;
+			new->parent = left;
+		}
+		else
+		{
+			new->parent = parent;
+			parent->left = new;
+		}
+	}
+	while (new->parent != NULL)
+		new = new->parent;
+
+	return (new);
+}
