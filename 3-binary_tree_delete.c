0eb914aa3f3017be37a571d66cdd61667a3eb08a
Jordan Mpounza <3630@holbertonschool.com>
2022-03-22 08:44:00 +0000

    add task 3 and 4

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+
+/**
+* binary_tree_delete - clear binary tree
+* @tree - root of tree
+*/
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (!tree)
+		return;
+
+	if (tree->left)
+		binary_tree_delete(tree->left);
+	if (tree->right)
+		binary_tree_delete(tree->right);
+
+	free(tree);
+}
