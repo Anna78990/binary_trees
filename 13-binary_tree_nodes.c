a2404f1e52f4c65c9b16ca1c6c36685dfefd7184
Jordan Mpounza <3630@holbertonschool.com>
2022-03-22 12:55:25 +0000

    add task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,5 +1,10 @@
 #include "binary_trees.h"
 
+/**
+* binary_tree_nodes -
+* @tree: binary tree root
+* Return: number of node in tree
+*/
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (!tree)
