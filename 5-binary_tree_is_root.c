f61afcf71a20071ca19ea893bf86cfb65fb72fc2
Jordan Mpounza <3630@holbertonschool.com>
2022-03-22 08:47:03 +0000

    simplify isroot

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -2,11 +2,8 @@
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node)
-	{
-		if (node->parent == NULL)
-			return (1);
-		return (0);
-	}
+	if (node && !node->parent)
+		return (1);
+
 	return (0);
 }
