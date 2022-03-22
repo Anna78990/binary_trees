5963bd44b86ecd4f8a68f8710fc6ed2cf37a5642
Anna78990 <takasuanna1990@gmail.com>
2022-03-22 16:26:26 +0000

    11-binary_tree_size.c

--- binary_trees.h
+++ binary_trees.h
@@ -32,5 +32,7 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree);
+void size_count(const binary_tree_t *tree, int *counter);
 
 #endif /* _BINARY_TREES_H_ */
