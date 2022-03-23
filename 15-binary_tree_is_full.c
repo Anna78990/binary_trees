0ccc676f2315d21d4ac772783603982f259d230a
Jordan Mpounza <3630@holbertonschool.com>
2022-03-23 11:14:26 +0000

    fix betty error task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -15,7 +15,8 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (1);
 
 	if ((tree->right) && (tree->left))
-		return ((binary_tree_is_full(tree->right) && binary_tree_is_full(tree->left)));
+		return ((binary_tree_is_full(tree->right) &&
+				binary_tree_is_full(tree->left)));
 
 	return (0);
 }
