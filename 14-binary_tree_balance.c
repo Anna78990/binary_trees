3ee5440e27f328f3d7cd409a8b1283b3b7d298c1
Jordan Mpounza <3630@holbertonschool.com>
2022-03-23 09:31:17 +0000

    fix error in task 14, add condition for tree==NULL

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -28,7 +28,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 * @tree: root of the tree
 * Return: The value of the balance into 2 branch
 */
+
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	if (!tree)
+		return (0);
+
 	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
