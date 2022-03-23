27db22930c3d7463ad18140d73db02bf7facf2a8
Jordan Mpounza <3630@holbertonschool.com>
2022-03-23 09:23:31 +0000

    add description and fix error betty on task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - measures the height of a binary tree
- * @tree: pointer to tree
- * Return: height, 0 if tree is NULL
- */
+* binary_tree_height - measures the height of a binary tree
+* @tree: pointer to tree
+* Return: height, 0 if tree is NULL
+*/
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
@@ -12,18 +12,22 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
+
+	left_side = binary_tree_height(tree->left);
+	right_side = binary_tree_height(tree->right);
+
+	if (left_side >= right_side)
+		return (left_side + 1);
 	else
-	{
-		left_side = binary_tree_height(tree->left);
-		right_side = binary_tree_height(tree->right);
-	
-		if (left_side >= right_side)
-			return left_side + 1;
-		else
-			return right_side + 1;
-	}
+		return (right_side + 1);
 }
 
+
+/**
+* binary_tree_balance - check the balance of the tree
+* @tree: root of the tree
+* Return: The value of the balance into 2 branch
+*/
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
