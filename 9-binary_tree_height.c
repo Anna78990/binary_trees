9d8194c74175a0ed5ef952acea28a3b06ca111d5
Jordan Mpounza <3630@holbertonschool.com>
2022-03-23 10:53:08 +0000

    fix function height, with the condition is leaf

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,7 +10,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	int left_side, right_side;
 
-	if (!tree)
+	if (!tree || (!tree->left && !tree->right))
 		return (0);
 
 	left_side = binary_tree_height(tree->left);
