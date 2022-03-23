bacced4de802701c4dd4eae761c033d12bb3aaf9
Jordan Mpounza <3630@holbertonschool.com>
2022-03-23 09:54:46 +0000

    fix recursion on function binary_tree_node

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,14 +1,22 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_nodes -
+* binary_tree_nodes - count number of nodes with at least 1
+* child in a binary tree
 * @tree: binary tree root
 * Return: number of node in tree
 */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+	size_t left_side, right_side;
+
 	if (!tree)
 		return (0);
 
-	return (+ 1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	left_side = binary_tree_nodes(tree->left);
+	right_side = binary_tree_nodes(tree->right);
+
+	if (tree->left || tree->right)
+		return (left_side + right_side + 1);
+	return (0);
 }
